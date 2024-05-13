11ae632fb24666622086c4fd3bf2386e411f2079
soufiendouzi <sdouzy@gmail.com>
2024-05-13 17:30:05 +0300

    fifa player

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PlayersList from './Component/PlayersList';
+
+function App() {
+  return (
+    <div className="App">
+           <h1 style={{ textAlign: 'center' }}> TOP Football Players in World</h1>
+
+<PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/players.js
@@ -0,0 +1,36 @@
+const player = [
+     {        name : "ronaldo" ,
+              team :"madrid" , 
+              nationality :"portugais",
+              jerseyNumber :"7" ,
+              age :"28" , 
+              imageUrl :"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goal.com%2Fen-in%2Flists%2Fcristiano-ronaldo-message-former-club-real-madrid-special-anniversary-with-photo%2Fblt00544d6a639446a9&psig=AOvVaw2zZI54wtLY8CTinpnhDnN9&ust=1715703855405000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCPDc0ZDqioYDFQAAAAAdAAAAABAE"
+
+    },
+     {
+        name :"mehrez" ,
+        team :"ManCity" , 
+        nationality :"algerien" ,
+        jerseyNumber :"26" ,
+        age :"28" , 
+        imageUrl :"https://www.google.com/url?sa=i&url=https%3A%2F%2Farabic.cnn.com%2Fsport%2Farticle%2F2018%2F07%2F11%2Fmahrez-man-city-algeria&psig=AOvVaw1Sjgg2oJkPt5u8oeWbRgrl&ust=1715703903355000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKDj0qnqioYDFQAAAAAdAAAAABAE"
+    } ,
+     { name :"Ozil" ,
+    team : "madrid" , 
+    nationality: "turkey",
+    jerseyNumber :"10" ,
+    age :"28" , 
+    imageUrl :"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamyimages.fr%2Fphoto-image-mesut-ozil-real-madrid-vs-malaga-au-santiago-bernabeu-madrid-espagne-03-01-12-59600927.html&psig=AOvVaw20bnBKmDDvbubvcZj_PamY&ust=1715703957231000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMDpucLqioYDFQAAAAAdAAAAABAE"
+
+    },
+     {
+        name :"Msalah" ,
+        team :"liverpool" , 
+        nationality  :"Egypt",
+        jerseyNumber :"11" ,
+        age :"28" , 
+        imageUrl : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FLiverpoolFC%2Fposts%2Fmohamed-salah-has-been-nominated-for-the-pfa-premier-league-player-of-the-month-%2F10157610775392573%2F&psig=AOvVaw0AmEMNu2sLLc08Uo4xyi95&ust=1715704040701000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCOjS4-jqioYDFQAAAAAdAAAAABAE"
+    }
+]
+
+export default player 
